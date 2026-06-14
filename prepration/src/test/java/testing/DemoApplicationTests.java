1de609dd9fc412cfd60af99770b75e41d23a0224
Bhanu Pratap <bhanu.mmmec2013@gmail.com>
2026-06-14 17:37:42 +0530

    repeted merge ,ss and is sort

---
+++ prepration/src/test/java/testing/DemoApplicationTests.java
@@ -0,0 +1,12 @@
+package testing;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DemoApplicationTests {
+
+    @Test
+    void addsTwoNumbers() {
+        assertEquals(4, 2 + 2);
+    }
+}
